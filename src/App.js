2ac388e8dd019256be4df29c5ecf5eee63f11303
SarraJEE <bgrsarra@gmail.com>
2023-12-18 22:37:43 +0100

    cration des Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
-
+import { Routes, Route,  BrowserRouter } from "react-router-dom";
 import "./sass/index.scss";
-
+import Home from "./pages/Home";
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import NoFound from "./pages/NoFound/NoFound";
 
 const App = () => {
   return (
-    <main>
+    <BrowserRouter>
       <Header />
 
-    
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NoFound />} />
+       
+      </Routes>
 
 
       <Footer />
-    </main>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/NoFound/NoFound.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+const NoFound = () => {
+  return (
+    <>
+      <div className="NoFound">
+       
+        <main>
+         <h1>page erreur</h1>
+        </main>
+
+      </div>
+    </>
+  );
+};
+
+export default NoFound;
