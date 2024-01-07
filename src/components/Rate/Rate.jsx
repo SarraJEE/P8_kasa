35dcf08c350a5b29389aabd19623e9f88d3896b6
SarraJEE <bgrsarra@gmail.com>
2024-01-07 21:57:33 +0100

    MAJ des composants  Rate et Logement

--- src/components/Rate/Rate.jsx
+++ src/components/Rate/Rate.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
-import emptyStar from "../../assets/images/rate/startRempli.svg";
-import fullStar from "../../assets/images/rate/StarEmpty.svg";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function Rate(props) {
-      const score = props.score;
-    const notes = [1, 2, 3, 4, 5];
-    return (
-      <div className="rate-contenair">
-        {notes.map((note) =>
-          score >= note ? (
-            <img
-              key={note.toString()}
-              className="etoile"
-              src={emptyStar}
-              alt="star"
-            />
-          ) : (
-            <img
-              key={note.toString()}
-              className="etoile"
-              src={fullStar}
-              alt="star"
-            />
-          )
-        )}
-      </div>
-    );
+  const score = props.score;
+  const notes = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="rate-contenair">
+      {notes.map((note) => (
+        <FontAwesomeIcon
+          icon={faStar}
+          className={score >= note ? 'star_red' : 'star'}
+          key={note.toString()}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Rate;
