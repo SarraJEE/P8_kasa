f91169c8e9a47b6167c8e750bb9d5696511a2850
SarraJEE <bgrsarra@gmail.com>
2024-01-07 20:08:08 +0100

    Modification de la strecture de projet et Adaptation de Scss collapse

--- src/pages/NoFound/NoFound.jsx
+++ src/pages/NoFound/NoFound.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const NoFound = () => {
   return (
     <>
-        <div className="errorContainer">
+      <div className="errorContainer">
         <p className="errorContainer__Number">404</p>
         <p className="errorContainer__Text">
           Oups! La page que vous demandez n'existe pas.
@@ -14,7 +14,7 @@ const NoFound = () => {
           Retourner sur la page d'accueil
         </Link>
       </div>
-   
+
     </>
   );
 };
